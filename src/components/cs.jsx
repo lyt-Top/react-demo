9b97a650f789d2c300189a32a6fc2c2b58d76359
lyt <lyt.github@gmail.com>
2019-08-27 16:49:11 +0800

    'submit'

--- src/components/cs.jsx
+++ src/components/cs.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 
-export default class Cs extends React.Component { 
-    componentDidMount() { 
+export default class Cs extends React.Component {
+    componentDidMount() {
         console.log(this.props.match)
     }
-    render() { 
+    render() {
         return (
-            <div>我是测试路由</div>
+            <div>
+                <p className="title">我是一个标题</p>
+                <p className="name">我是测试路由</p>
+            </div>
         )
     }
 }
